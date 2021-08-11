02df711c6b80c32c11d5a277674233e512944a1a
노다혜 <ddhh3@naver.com>
2021-08-11 12:17:35 +0900

    Initial commit

---
+++ src/main/java/com/math/DHJmath/DhJmathApplication.java
@@ -0,0 +1,13 @@
+package com.math.DHJmath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhJmathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhJmathApplication.class, args);
+	}
+
+}
